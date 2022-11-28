f7968e5e63cea883bb76f9d30cb391fde2ab7f46
RstmZ <rstm.ramaz@gmail.com>
2022-11-28 21:09:26 +0200

    add project

---
+++ src/rateLimit.ts
@@ -0,0 +1,29 @@
+import { config } from 'dotenv';
+
+config();
+
+const MAX_REQUESTS: number = parseInt(String(process.env.MAX_REQUESTS), 10);
+const INTERVAL: number = parseFloat(String(process.env.INTERVAL));
+
+export class RateLimit {
+  count: number;
+  constructor() {
+    this.count = 0;
+  }
+
+  async check() {
+    if (this.count >= MAX_REQUESTS) { 
+      return false;
+    }
+    this.count++;
+    return true;
+  }
+
+  async startTime() {
+    console.log(INTERVAL * 1000);
+    
+    setTimeout(()=>{
+      this.count = 0
+    }, INTERVAL * 1000)
+  }
+}
